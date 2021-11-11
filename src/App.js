d040abfce1b54536d1c84ccc7bda2e08c4e17627
respawner8 <nikhil8.anand@gmail.com>
2021-11-11 20:54:22 +0530

    Add support for dotenv

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+require("dotenv").config();
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit {process.env.REACT_APP_BACKEND_URL}
         </a>
       </header>
     </div>
